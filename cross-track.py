46330a8c713e422ebc65e0255e434170e07cc75a
jacob-m122 <123427189+jacob-m122@users.noreply.github.com>
2026-05-15 17:59:18 -0700

    initial commit

---
+++ cross-track.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+
+class AudioStemDataset(Dataset):
+    def __init__(self, file_paths, sample_rate=44100, n_fft=2048):
+        self.file_paths = file_paths
+        self.n_fft = n_fft
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx):
+        pass
+    
+class CrossTrackAttention(nn.Module):
+    def __init__(self, embed_dim):
+        super(CrossTrackAttention, self).__init__()
+        self.query = nn.Linear(embed_dim, embed_dim)
+        self.key = nn.Linear(embed_dim, embed_dim)
+        self.value = nn.Linear(embed_dim, embed_dim)
+
+def forward(self, track_a, track_b):
+    q = self.query(track_a)
+    k = self.key(track_b)
+    v = self.value(track_b)
+
+    attn_weights = torch.matmul(q, k.transpose(-2, -1)) / (q.size(-1) ** 0.5)
+    soft_attn = F.softmax(attn_weights, dim=-1)
+
+    
+    

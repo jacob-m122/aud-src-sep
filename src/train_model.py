54da3ca1a6696bae99262cb576af118617e4a613
jacob-m122 <123427189+jacob-m122@users.noreply.github.com>
2026-06-08 20:00:12 -0700

    eval.ipynb updated for visualization

--- src/train_model.py
+++ src/train_model.py
@@ -19,11 +19,12 @@ val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_worke
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
-    print(f"Colab gpu detected: {torch.cuda.get_device_name(0)} ")
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
 else:
     device = torch.device("cpu")
-    print("no gpu detected")
-
+    
+print(f"Using device: {device}")
 
 
 model = AntiArtifactModel(embed_dim=128).to(device)

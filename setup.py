de2ae3763b202b9b61d1dd379e089b14885bc9db
jacob-m122 <123427189+jacob-m122@users.noreply.github.com>
2026-06-09 23:57:38 -0700

    setup and toml added

--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
 from setuptools import setup, find_packages
-setup(name='audio_separation_project', version='0.1.0', packages=find_packages())
+
+setup(
+    name='audio_separation_project',
+    version='0.1.0',
+    description='Audio Stem Source Separation via Cross-Track Attention',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchaudio',
+        'torchmetrics',
+        'librosa',
+        'matplotlib',
+        'soundfile',
+        'scipy',
+        'numpy',
+        'jupyter',
+        'kaggle'
+    ],
+)
